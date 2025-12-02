37760dc6fb66620cc149e3177232b52f19d94bf5
joao-vitorb <joaovborges2005@hotmail.com>
2025-12-02 18:31:12 -0300

    criação do formulário para adicionar novo usuário

--- client/src/addUser/AddUser.jsx
+++ client/src/addUser/AddUser.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React from "react";
+import "./adduser.css";
+import { Link } from "react-router-dom";
 
 const AddUser = () => {
   return (
-    <div>
-      Form Adicionar Usuário
-    </div>
-  )
-}
+    <div className="addUser">
+      <Link to="/" type="button" class="btn btn-secondary">
+        <i class="fa-solid fa-backward"></i>Voltar
+      </Link>
 
-export default AddUser
+      <h3>Adicionar novo usuário</h3>
+      <form className="addUserForm">
+        <div className="inputGroup">
+          <label htmlFor="name">Nome:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            autoComplete="off"
+            placeholder="Fulano da Silva"
+          />
+        </div>
+        <div className="inputGroup">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            autoComplete="off"
+            placeholder="exemplo@email.com"
+          />
+        </div>
+        <div className="inputGroup">
+          <label htmlFor="address">Endereço:</label>
+          <input
+            type="text"
+            id="address"
+            name="address"
+            autoComplete="off"
+            placeholder="Rua Fictícia, 123, Bairro Fictício"
+          />
+        </div>
+        <div className="inputGroup">
+          <button type="button" class="btn btn-primary">
+            Adicionar
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AddUser;
