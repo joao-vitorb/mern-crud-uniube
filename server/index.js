f348679f6c78e304f94038cdd344b16279c9485c
joao-vitorb <joaovborges2005@hotmail.com>
2025-12-02 16:04:00 -0300

    inserção de dados no mongodb

--- server/index.js
+++ server/index.js
@@ -2,13 +2,14 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import route from "./routes/userRoute.js";
 
 const app = express();
 app.use(bodyParser.json());
 dotenv.config();
 
 const PORT = process.env.PORT || 7000;
-const MONGO_URL = process.env.MONGO_URL;
+const MONGOURL = process.env.MONGO_URL;
 
 mongoose
   .connect(MONGOURL)
@@ -19,3 +20,5 @@ mongoose
     });
   })
   .catch((error) => console.log(error));
+
+app.use("/api", route);
