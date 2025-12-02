d33afc3b011ee423a0480100b1ea4a937f480d9f
joao-vitorb <joaovborges2005@hotmail.com>
2025-12-02 17:52:57 -0300

    criação da tabela de usuários utilizando bootstrap

--- client/src/getUser/User.jsx
+++ client/src/getUser/User.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import "./user.css"
 
 const User = () => {
   return (
-    <div>
-      Tabela de Usuários
+    <div className='userTable'>
+        <button type='button' class="btn btn-primary">Adicionar usuário</button>
+      <table className='table table-bordered'>
+        <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Email</th>
+                <th scope="col">Endereço</th>
+                <th scope="col">Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>João</td>
+                <td>joao@gmail.com</td>
+                <td>Brasil</td>
+                <td>Editar | Deletar</td>
+            </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
