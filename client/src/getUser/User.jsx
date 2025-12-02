01071346fb9fdf2c508846148ea046d854827d6d
joao-vitorb <joaovborges2005@hotmail.com>
2025-12-02 17:58:53 -0300

    adição de icones utilizando font-awesome

--- client/src/getUser/User.jsx
+++ client/src/getUser/User.jsx
@@ -1,32 +1,41 @@
-import React from 'react'
-import "./user.css"
+import React from "react";
+import "./user.css";
 
 const User = () => {
   return (
-    <div className='userTable'>
-        <button type='button' class="btn btn-primary">Adicionar usuário</button>
-      <table className='table table-bordered'>
+    <div className="userTable">
+      <button type="button" class="btn btn-primary">
+        Adicionar usuário <i class="fa-solid fa-user-plus"></i>
+      </button>
+      <table className="table table-bordered">
         <thead>
-            <tr>
-                <th scope="col">Id</th>
-                <th scope="col">Nome</th>
-                <th scope="col">Email</th>
-                <th scope="col">Endereço</th>
-                <th scope="col">Ações</th>
-            </tr>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Email</th>
+            <th scope="col">Endereço</th>
+            <th scope="col">Ações</th>
+          </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>1</td>
-                <td>João</td>
-                <td>joao@gmail.com</td>
-                <td>Brasil</td>
-                <td>Editar | Deletar</td>
-            </tr>
+          <tr>
+            <td>1</td>
+            <td>João</td>
+            <td>joao@gmail.com</td>
+            <td>Brasil</td>
+            <td className="actionButtons">
+              <button type="button" class="btn btn-info">
+                <i class="fa-solid fa-pen-to-square"></i>
+              </button>
+              <button type="button" class="btn btn-danger">
+                <i class="fa-solid fa-trash"></i>
+              </button>
+            </td>
+          </tr>
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default User
+export default User;
