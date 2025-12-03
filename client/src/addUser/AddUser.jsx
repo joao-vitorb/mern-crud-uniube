5be775633200447a24614095247b4b19b59336df
joao-vitorb <joaovborges2005@hotmail.com>
2025-12-03 15:17:38 -0300

    registro de dados no banco utilizando axios

--- client/src/addUser/AddUser.jsx
+++ client/src/addUser/AddUser.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./adduser.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const AddUser = () => {
+  const users = {
+    name: "",
+    email: "",
+    address: "",
+  };
+  const [user, setUser] = useState(users);
+  const navigate = useNavigate();
+
+  const inputHandler = (e) => {
+    const { name, value } = e.target;
+    console.log(name, value);
+    setUser({ ...user, [name]: value });
+  };
+
+  const submitForm = async (e) => {
+    e.preventDefault();
+    await axios
+      .post("http://localhost:8000/api/user", user)
+      .then((response) => {
+        console.log("Usuário criado com sucesso");
+        navigate("/");
+      })
+
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="addUser">
       <Link to="/" type="button" class="btn btn-secondary">
@@ -10,12 +39,13 @@ const AddUser = () => {
       </Link>
 
       <h3>Adicionar novo usuário</h3>
-      <form className="addUserForm">
+      <form className="addUserForm" onSubmit={submitForm}>
         <div className="inputGroup">
           <label htmlFor="name">Nome:</label>
           <input
             type="text"
             id="name"
+            onChange={inputHandler}
             name="name"
             autoComplete="off"
             placeholder="Fulano da Silva"
@@ -26,6 +56,7 @@ const AddUser = () => {
           <input
             type="text"
             id="email"
+            onChange={inputHandler}
             name="email"
             autoComplete="off"
             placeholder="exemplo@email.com"
@@ -36,13 +67,14 @@ const AddUser = () => {
           <input
             type="text"
             id="address"
+            onChange={inputHandler}
             name="address"
             autoComplete="off"
             placeholder="Rua Fictícia, 123, Bairro Fictício"
           />
         </div>
         <div className="inputGroup">
-          <button type="button" class="btn btn-primary">
+          <button type="submit" class="btn btn-primary">
             Adicionar
           </button>
         </div>
